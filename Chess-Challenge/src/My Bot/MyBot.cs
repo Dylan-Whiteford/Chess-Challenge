4ef9160011aa0ccc647201c9abb274d8a6aea176
Dylan Whiteford <dylan.whiteford@southerncross.solutions>
2023-08-03 21:25:28 +0200

    ✨ Makes AI pick random moves

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,13 @@
 ﻿using ChessChallenge.API;
-
+using System;
 public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
+
+        Random rng = new();
         Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        Move moveToPlay = moves[rng.Next(moves.Length)];
+        return moveToPlay;
     }
 }
