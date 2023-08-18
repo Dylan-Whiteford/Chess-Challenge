99173a050908b341c920d03bda29ba6ca2c99932
werner-scholtz <werner.scholtz@kdab.com>
2023-08-18 18:49:46 +0200

    Cleanup and docs

--- Chess-Challenge/src/My Bot/MinMax.cs
+++ Chess-Challenge/src/My Bot/MinMax.cs
@@ -1,60 +1,43 @@
 using ChessChallenge.API;
 using System;
-using System.Diagnostics;
+
 
 namespace MinMaxClasses
 {
-
-
-
     /// <summary>
     /// Implementation of the Minimax algorithm with alpha-beta pruning.
     /// </summary>
     public class MinMaxWithAlphaPruning
     {
 
-
-
         /// <summary>
-        /// Returns the best move for the current player.
+        /// Returns the best move for the current player. 
         /// 
-        /// TODO(Werner): Keep an tree of the best moves, so that in the case that the opponent makes a move that we
-        ///               have already evaluated, we can continue with the tree. (Not sure this is possible)
         /// </summary>
-        /// <param name="board"></param>
-        /// <param name="depth"></param>
-        /// <returns></returns>
+        /// <param name="board">The current board</param>
+        /// <param name="depth">The depth of the search (Must be greater than 0)</param>
+        /// <returns>The best move </returns>
         public Move FindBestMove(Board board, int depth)
         {
 
-            Debug.Assert(depth > 0, "Depth must be greater than 0.");
-
             int bestEvaluation = int.MinValue;
             Move bestMove = Move.NullMove;
-
             int alpha = int.MinValue;
             int beta = int.MaxValue;
-
 
             // use GetLegalMovesNonAlloc(ref moves, bool capturesOnly) to avoid allocating a new array every time.
             // see https://seblague.github.io/chess-coding-challenge/documentation/ 
             Span<Move> legalMoves = stackalloc Move[256];
             board.GetLegalMovesNonAlloc(ref legalMoves, capturesOnly: false);
 
+            // Loop through all legal moves.
             foreach (Move move in legalMoves)
-            {   
+            {
                 // Make the move.
                 board.MakeMove(move);
 
-                // Console.WriteLine($"Base Move: {move.MovePieceType}");
-            
                 // Evaluate the board with the Minimax algorithm.
-                int evaluation = AlphaBeta(board, depth - 1,  alpha,  beta,  board.IsWhiteToMove);
-
-                //  Console.WriteLine($"Alpha: {alpha}, Beta: {beta}");
-
-                // Console.WriteLine($"eval: {evaluation}");
-                // Console.WriteLine($"___________");
+                int evaluation = AlphaBeta(board, depth - 1, alpha, beta, board.IsWhiteToMove);
 
                 // TODO:(discussion) What to do with equal evals?
                 if (evaluation > bestEvaluation)
@@ -78,36 +61,34 @@ namespace MinMaxClasses
         /// The algorithm is based on the pseudocode from https://en.wikipedia.org/wiki/Alpha%E2%80%93beta_pruning
         /// 
         /// </summary>
-        /// <param name="board"></param>
-        /// <param name="depth"></param>
-        /// <param name="alpha"></param>
-        /// <param name="beta"></param>
-        /// <param name="maximizingPlayer"></param>
-        /// <returns></returns>
+        /// <param name="board">The board</param>
+        /// <param name="depth">The depth</param>
+        /// <param name="alpha">The alpha value</param>
+        /// <param name="beta">The beta value</param>
+        /// <param name="maximizingPlayer">Is the maximizing player</param>
+        /// <returns>The evaluation of the board at the depth</returns>
         private int AlphaBeta(Board board, int depth, int alpha, int beta, bool maximizingPlayer)
         {
 
             Span<Move> legalMoves = stackalloc Move[256];
             board.GetLegalMovesNonAlloc(ref legalMoves, capturesOnly: false);
-            
-      
-            if (depth == 0)  return new Evaluation().Evaluate(board);
-            
 
+            // If the depth is 0, return the evaluation of the board.
+            if (depth == 0) return new Evaluation().Evaluate(board);
 
             if (maximizingPlayer)
             {
                 int value = int.MinValue;
 
                 // If the game is over, return the minimum evaluation since we don't want to lose/draw.
-                //if (board.IsInCheckmate() || board.IsDraw()) return value;
+                if (board.IsInCheckmate() || board.IsDraw()) return value;
 
-              
+
                 foreach (Move move in legalMoves)
                 {
                     board.MakeMove(move);
 
-                    value = Math.Max(value, AlphaBeta(board, depth - 1,  alpha,  beta, false));
+                    value = Math.Max(value, AlphaBeta(board, depth - 1, alpha, beta, false));
 
                     alpha = Math.Max(alpha, value);
 
@@ -124,12 +105,13 @@ namespace MinMaxClasses
                 int value = int.MaxValue;
 
                 // If the game is over, return the maximum value beause the opponent doesn't want to lose/draw.
-                //if (board.IsInCheckmate() || board.IsDraw()) return minEvaluation;
+                if (board.IsInCheckmate() || board.IsDraw()) return value;
+
                 foreach (Move move in legalMoves)
                 {
                     board.MakeMove(move);
 
-                    value = Math.Min(value, AlphaBeta(board, depth  - 1,  alpha,  beta, true));
+                    value = Math.Min(value, AlphaBeta(board, depth - 1, alpha, beta, true));
                     beta = Math.Min(beta, value);
                     board.UndoMove(move);
 
@@ -139,108 +121,6 @@ namespace MinMaxClasses
 
                 return value;
             }
-        }
-
-    }
-
-    /// <summary>
-    /// Basic implementation of the Minimax algorithm.
-    /// </summary>
-    public class MinMax
-    {
-
-        /// <summary>
-        /// Returns the best move for the current player.
-        /// </summary>
-        /// <param name="board"></param>
-        /// <param name="depth"></param>
-        /// <returns></returns>
-        public Move FindBestMove(Board board, int depth)
-        {
-            // Assert that the depth is greater than 0.
-            Debug.Assert(depth > 0, "Depth must be greater than 0.");
-
-            int bestEval = int.MinValue;
-            Move bestMove = Move.NullMove;
-
-            foreach (Move move in board.GetLegalMoves(capturesOnly: false))
-            {
-                board.MakeMove(move);
-
-                int evaluation = Minimize(board, depth - 1);
-
-                if (evaluation > bestEval)
-                {
-                    bestEval = evaluation;
-                    bestMove = move;
-                }
-
-                board.UndoMove(move);
-            }
-
-            Debug.Assert(bestMove != Move.NullMove, "bestMove is null, because there is no valid move.");
-
-            return bestMove;
-        }
-
-        private int Maximize(Board board, int depth)
-        {
-
-            if (depth == 0) return new Evaluation().Evaluate(board);
-
-            int maximumValue = int.MinValue;
-
-            // If the game is over, return int.MinValue.
-            if (board.IsInCheckmate() || board.IsDraw()) return maximumValue;
-
-
-            foreach (Move move in board.GetLegalMoves(capturesOnly: false))
-            {
-                // Make the move.
-                board.MakeMove(move);
-
-                // Evaluate the board.
-                int eval = Minimize(board, depth - 1);
-
-                // Update the maxEval.
-                maximumValue = Math.Max(maximumValue, eval);
-
-                // Undo the move.
-                board.UndoMove(move);
-            }
-
-            return maximumValue;
-        }
-
-
-        private int Minimize(Board board, int depth)
-        {
-
-
-            if (depth == 0) return new Evaluation().Evaluate(board);
-
-            int minimumValue = int.MaxValue;
-
-            // If the game is over, return int.MaxValue.
-            if (board.IsInCheckmate() || board.IsDraw()) return minimumValue;
-
-
-            foreach (Move move in board.GetLegalMoves(capturesOnly: false))
-            {
-                // Make the move.
-                board.MakeMove(move);
-
-                // Evaluate the board.
-                int eval = Maximize(board, depth - 1);
-
-                // Update the minEval.
-                minimumValue = Math.Min(minimumValue, eval);
-
-                // Undo the move.
-                board.UndoMove(move);
-            }
-
-            return minimumValue;
         }
 
     }
@@ -281,7 +161,7 @@ namespace MinMaxClasses
             int blackScore = bpawns + bknights + bbishops + brooks + bqueens + bkings;
 
             int score;
-            score = whiteScore -  blackScore;
+            score = whiteScore - blackScore;
 
             return score;
         }
--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,21 +1,21 @@
-﻿using ChessChallenge.API;
+﻿using System;
+using ChessChallenge.API;
 using MinMaxClasses;
 
 public class MyBot : IChessBot
 {
-    public Move Think(Board board, Timer timer)
-    {   
 
+    public Move Think(Board board, Timer timer)
+    {
 
         // AlphaBetaPruning example
         MinMaxWithAlphaPruning minimax = new();
         Move moveToPlay = minimax.FindBestMove(board, 4);
 
-
-        // MinMax example
-        // MinMax minimax = new();
-        // Move moveToPlay = minimax.FindBestMove(board, 6);
-
         return moveToPlay;
     }
+
+
 }
+
+
